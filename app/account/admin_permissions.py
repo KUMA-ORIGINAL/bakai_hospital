d2a38a85670c061a1082e8f2111306e38556a00c
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-03-12 14:19:27 +0600

    init

--- app/account/admin_permissions.py
+++ app/account/admin_permissions.py
@@ -7,6 +7,8 @@ def permission_callback(request):
     return False
 
 def permission_callback_for_doctor_and_accountant(request):
+    if request.user.is_superuser:
+        return True
     if request.user.role in (ROLE_DOCTOR, ROLE_ACCOUNTANT):
         return False
     return True
