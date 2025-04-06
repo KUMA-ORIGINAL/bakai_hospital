b2dac3d9eb8c01c808fa211671bda03433c7182f
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-04-06 17:59:25 +0600

    refact: services.json

--- app/transactions/services/open_banking_bakai.py
+++ app/transactions/services/open_banking_bakai.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 from config.settings import DOMAIN
 
-PAYMENT_API_URL = "https://openbanking-api.bakai.kg/api/PayLink/CreatePayLink"
+PAYMENT_API_URL = "http://193.176.239.186/api/v1/payments/make-payment-link/"
 PAYMENT_API_TOKEN = settings.PAYMENT_API_TOKEN
 
 logger = logging.getLogger(__name__)
@@ -15,22 +15,22 @@ logger = logging.getLogger(__name__)
 def generate_payment_link(transaction):
     payload = {
         "amount": str(transaction.total_price),  # Итоговая сумма заказа
-        "transactionID": str(transaction.id),  # ID заказа
+        "transaction_id": str(transaction.id),  # ID заказа
         "comment": f"Оплата заказа #{transaction.id}",  # Комментарий
-        "redirectURL": f"https://hospital.mnogo.kg/api/payment/webhook/"  # URL после успешной оплаты
+        "redirect_url": f"https://hospital.mnogo.kg/api/payment/webhook/",
+        'token': PAYMENT_API_TOKEN,
     }
 
     headers = {
         "Content-Type": "application/json",
-        'accept': '*/*',
-        'Authorization': f"Bearer {PAYMENT_API_TOKEN}"
     }
 
     try:
         response = requests.post(PAYMENT_API_URL, json=payload, headers=headers)
 
         if response.status_code == 200:
-            return response.text.strip()
+            data = response.json()
+            return data.get('pay_url')
         else:
             logger.error(f"Ошибка создания платёжной ссылки. Код: {response.status_code}, Ответ: {response.content}")
             return None
