f771410f6ee42f663e40790e13a43a5c8ba807d1
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-05-04 14:05:32 +0600

    refact: doctor permission

--- app/organizations/urls.py
+++ app/organizations/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import RoomViewSet
+from .views import RoomViewSet, OrganizationViewSet
 
 
 router = DefaultRouter()
 router.register(r'rooms', RoomViewSet)
-router.register(r'organizations', RoomViewSet)
+router.register(r'organizations', OrganizationViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
