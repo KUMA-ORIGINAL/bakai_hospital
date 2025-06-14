84a0a0be2f48d5778c0ccc8ca60e7e40b1f89b48
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-06-14 20:37:24 +0600

    fix: get and create transaction

--- nginx/nginx_entrypoint.sh
+++ nginx/nginx_entrypoint.sh
@@ -3,24 +3,21 @@
 # Приводим GET_CERTS к нижнему регистру
 get_certs_lower=$(echo "$GET_CERTS" | tr '[:upper:]' '[:lower:]')
 
-# Если GET_CERTS=true
 if [ "$get_certs_lower" = "true" ]; then
 
     domains="$DOMAIN $CORE_DOMAIN"
 
     for domain in $domains; do
-#        folder_path="/etc/letsencrypt/live/$domain"
-
-#        if [ -d "$folder_path" ]; then
-#            echo "🔁 Сертификат уже есть для $domain, обновляем..."
-#            certbot -n --nginx -d "$domain" -d "www.$domain"
-#        else
-        echo "🆕 Получаем новый сертификат для $domain..."
-        certbot --nginx --email "$CERTBOT_EMAIL" --agree-tos --no-eff-email -d "$domain"
-#        fi
+        echo "🆕 Получаем или обновляем сертификат для $domain..."
+        certbot --nginx \
+            --email "$CERTBOT_EMAIL" \
+            --agree-tos \
+            --no-eff-email \
+            -d "$domain" -d "www.$domain" \
+            --non-interactive
     done
 
-    # Перезапуск nginx после получения всех сертификатов
+    # Перезапуск nginx после получения всех сертификатов (на всякий случай)
     echo "🔄 Перезапуск nginx..."
     nginx -s stop
     sleep 2
